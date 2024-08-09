7c118aea82ae9a6a9c7a856d76c08b178641ab68
dRadiant <103012174+dRadiant@users.noreply.github.com>
2024-08-09 20:55:12 +0000

    Add updateRoot script

---
+++ script/UpdateUNRewardsRoot.s.sol
@@ -0,0 +1,20 @@
+// SPDX-LICENSE-IDENTIFIER: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/UNRewardsRedemption.sol";
+import "../src/UN.sol";
+
+contract DeployUNRewardsRedemption is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        UNRewardsRedemption redemption = UNRewardsRedemption(0x99D8f6CF4dE68ead2392B74e3dD8485E4f74336F);
+        bytes32 root;
+
+        redemption.updateRoot(root);
+
+        vm.stopBroadcast();
+    }
+}
